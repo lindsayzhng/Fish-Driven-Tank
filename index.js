65361cea2051181919dffd59f7c772c37f8a2e26
Aaron Olsen <aaronolsen11@gmail.com>
2022-11-16 17:42:06 -0500

    Create simple drive

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require('express');
-// const { init } = require('raspi');
-// const { SoftPWM } = require('raspi-soft-pwm');
+const { init } = require('raspi');
+const { SoftPWM } = require('raspi-soft-pwm');
 
 
+init(() => {
+    const rightMotorForward = new SoftPWM('GPIO18');
+    const rightMotorBackward = new SoftPWM('GPIO19');
+    const leftMotorForward = new SoftPWM('GPIO13');
+    const leftMotorBackward = new SoftPWM('GPIO12');
 
-const app = express();
-const port = 3030;
+    const app = express();
+    const port = 3030;
 
-app.use(express.static('public'))
-app.use(express.json());
+    app.use(express.static('public'))
+    app.use(express.json());
 
-app.post('/drive', (req, res) => {
-    console.log(req.body);
-    res.send(200);
-})
+    app.post('/drive', (req, res) => {
+        const { rf, rb, lf, lb } = req.body;
 
-app.listen(port, () => {
-    console.log(`Capy listening on port ${port}`)
+        rightMotorForward.write(rf ?? 0);
+        rightMotorBackward.write(rb ?? 0);
+        leftMotorForward.write(lf ?? 0)
+        leftMotorBackward.write(lb ?? 0);
+
+        res.send(200);
+
+    })
+
+    app.listen(port, () => {
+        console.log(`Capy listening on port ${port}`)
+    });
 });
+
+
