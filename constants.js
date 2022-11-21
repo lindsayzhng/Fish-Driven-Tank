63c80637f8e31239f0d6b2b28b010804d39034e9
Nicole Jin <nicole.qianhui.jin@gmail.com>
2022-11-21 03:41:17 -0500

    Create constants file

---
+++ constants.js
@@ -0,0 +1,19 @@
+// change file to type module in html
+
+export const Motors = {
+
+    RIGHT_FORWARD: 'GPIO18',
+    RIGHT_BACKWARD: 'GPIO19',
+    LEFT_FORWARD: 'GPIO13',
+    LEFT_BACKWARD: 'GPIO12',
+
+    MAX_VOLTAGE: 0.3, // max power: 30%
+
+};
+
+export const Raw = { // up to change
+
+    INPUT_DEADBAND: 0.05,
+    DRIVE_SPEED_MULT: 5,
+
+};
--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require('express');
-// const { init } = require('raspi');
-// const { SoftPWM } = require('raspi-soft-pwm');
+const { SoftPWM } = require('raspi-soft-pwm');
+const { init } = require('raspi');
+const constants = require('./constants');
 
+init(() => {
 
+    const app = express();
+    const port = 3030;
 
-const app = express();
-const port = 3030;
+    const rightMotorForward = new SoftPWM(constants.Motors.RIGHT_FORWARD);
+    const rightMotorBackward = new SoftPWM(constants.Motors.RIGHT_BACKWARD);
+    const leftMotorForward = new SoftPWM(constants.Motors.LEFT_FORWARD);
+    const leftMotorBackward = new SoftPWM(constants.Motors.LEFT_BACKWARD);
 
-app.use(express.static('public'))
-app.use(express.json());
+    app.use(express.static('public'))
+    app.use(express.json());
 
-app.post('/drive', (req, res) => {
-    console.log(req.body);
-    res.send(200);
-})
+    app.post('/drive', (req, res) => {
 
-app.listen(port, () => {
-    console.log(`Capy listening on port ${port}`)
+        const { rawX, rawY } = req.body;
+        console.log(req.body);
+
+        // process raw
+
+        // write to motor
+
+        res.send(200);
+    })
+
+    app.listen(port, () => {
+        console.log(`Capy listening on port ${port}`)
+    });
 });
+
+// implement arcade drive / curvature drive
