2ead9ac6291dd5ed1c1446d18fdfb5a9f8cdf34e
Nicole Jin <nicole.qianhui.jin@gmail.com>
2022-11-24 11:21:03 -0500

    Integrate frontend and backend

--- public/index.js
+++ public/index.js
@@ -1,45 +1,23 @@
-lock = false;
-const el = document.getElementById("switch");
-el.addEventListener("click", function () {
-  if (lock == false) {
-    lock = true;
-    drive(0, 0, true, 'Joystick');
-    console.log('lock');
-  }
-  else {
-    lock = false;
-    drive(0, 0, false, 'Joystick');
-    console.log('unlock');
-  }
-})
+let lock = true;
 
-function drive(rawX, rawY, lock) {
-  if (lock == false) {
-    fetch("/drive", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ rawX, rawY }),
-    });
-  }
-  else {
-    fetch("/drive", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      rawX: 0,
-      rawY: 0,
-      body: JSON.stringify({ rawX, rawY }),
-    });
-  }
+function handleLock(el) {
+  lock = el.checked;
+  console.log(lock);
 }
 
 var joy = new JoyStick('joyDiv', {}, function (stickData) {
   stickData.x /= 100;
   stickData.y /= 100;
-  drive(stickData.x, stickData.y, 'Joystick');
+  drive(stickData.x, stickData.y, lock, 'Joystick');
 });
+
+function drive(rawX, rawY, lock, caller) {
+  fetch("/drive", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ rawX, rawY, lock, caller }),
+  });
+}
