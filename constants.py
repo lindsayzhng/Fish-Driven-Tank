48b7f55baebd44dc3eb9e5fcb42f3dc6c8ca91c1
Nicole Jin <nicole.qianhui.jin@gmail.com>
2022-11-28 10:59:45 -0500

    Create python constants file

---
+++ constants.py
@@ -0,0 +1,23 @@
+# max resolution for video - number of pixels
+FRAME_WIDTH = 1920
+FRAME_HEIGHT = 1080
+
+FRAMERATE = 15
+BRIGHTNESS = 50
+
+# coordinate of center of tank in respect to coordinate of center of image
+X_OFFSET = 0
+Y_OFFSET = 0
+
+GAUSSIAN_KERNEL = (5, 5)
+GAUSSIAN_SIGMA = 1
+
+CANNY_LOWER_THRESHOLD = 128
+CANNY_UPPER_THRESHOLD = 255
+
+DILATE_KERNEL = (5, 5)
+ERODE_KERNEL = (5, 5)
+
+CONTOUR_COLOR = (0, 255, 0)
+CONTOUR_THICKNESS = 2
+CONTOUR_MIN_AREA = 200
